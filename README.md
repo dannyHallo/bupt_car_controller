b0da444ebe9fb56edfba0ee59c591bc5de55b8a6
DannyHallo <ruitian0716@gmail.com>
2022-09-18 12:21:33 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# bupt_car_controller
+ 
---
+++ app/src/main/java/com/example/thedeadwaltz/AllCommands.java
@@ -0,0 +1,18 @@
+package com.example.thedeadwaltz;
+
+public class AllCommands {
+    static char COMMAND_L_WHEEL_FORWARD    = '0';
+    static char COMMAND_L_WHEEL_FORWARD_S  = '1';
+    static char COMMAND_L_WHEEL_BACKWARD   = '2';
+    static char COMMAND_L_WHEEL_BACKWARD_S = '3';
+    static char COMMAND_R_WHEEL_FORWARD    = '4';
+    static char COMMAND_R_WHEEL_FORWARD_S  = '5';
+    static char COMMAND_R_WHEEL_BACKWARD   = '6';
+    static char COMMAND_R_WHEEL_BACKWARD_S = '7';
+    static char COMMAND_START_LISTEN       = '8';
+    static char COMMAND_STOP_LISTEN        = '9';
+    static char COMMAND_BOARDLED_ON        = 'a';
+    static char COMMAND_BOARDLED_OFF       = 'b';
+    static char COMMAND_LOWER_FUEL         = 'c';
+    static char COMMAND_ADD_FUEL           = 'd';
+}
